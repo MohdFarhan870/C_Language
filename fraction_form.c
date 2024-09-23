56fd14b633944d442d2026854e78f32511146da8
MohdFarhan870 <mohdfarhan2135@gmail.com>
2024-09-23 19:38:52 +0530

    fraction_form.c

--- fraction_form.c
+++ fraction_form.c
@@ -1,30 +1,43 @@
 
 #include <stdio.h>
+
+// Function to find the Greatest Common Divisor (GCD)
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
 int main() {
-    int n, d, gcd, r, t;
-    printf("Enter numerator: ");
-    scanf("%d", &n);
-    printf("Enter denominator: ");
-    scanf("%d", &d);
-    if (d == 0) {
-        printf("Error: Division by zero.\n");
+    int numerator, denominator;
+    
+    // Input the numerator and denominator
+    printf("Enter the numerator: ");
+    scanf("%d", &numerator);
+    
+    printf("Enter the denominator: ");
+    scanf("%d", &denominator);
+    
+    // If the denominator is 0, the fraction is undefined
+    if (denominator == 0) {
+        printf("Denominator cannot be zero.\n");
         return 1;
     }
-    t = (n > d) ? n : d;
-    while (1) {
-        r = (n % d);
-        if (r == 0) {
-            gcd = d;
-            break;
-        }
-        n = d;
-        d = r;
-    }
-    n /= gcd;
-    d /= gcd;
-    if (d == 1){
-        printf("%d\n", n);}
-    else
-       { printf("%d/%d\n", n, d);}
-    return 0;
+    
+    // Get the GCD of the numerator and denominator
+    int divisor = gcd(numerator, denominator);
+    
+    // Simplify the fraction by dividing both by the GCD
+    int simpleNumerator = numerator / divisor;
+    int simpleDenominator = denominator / divisor;
+    
+    // Print the result
+    printf("%d/%d = %d/%d\n", numerator, denominator, simpleNumerator, simpleDenominator);
+    
+    return 0;
 }
+
+
